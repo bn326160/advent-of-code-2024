1c027e704410d93c058a12526e3e87a2d169568f
Bram Beirens <bram.beirens@be.ibm.com>
2024-12-01 22:50:41 +0100

    Day 1

---
+++ day-1-pt-1/Program.cs
@@ -0,0 +1,42 @@
+﻿var listOne = new List<int>();
+var listTwo = new List<int>();
+int lineCount = 0;
+
+try
+{
+    using StreamReader reader = new("input.txt");
+
+    string text = reader.ReadToEnd();
+
+    string[] lines = text.Split(
+      new string[] { Environment.NewLine },
+      StringSplitOptions.None
+    );
+    lineCount = lines.Count();
+
+    foreach (var line in lines)
+    {
+        string[] words = System.Text.RegularExpressions.Regex.Split( line, @"\s{2,}");
+        listOne.Add(Int32.Parse(words[0]));
+        listTwo.Add(Int32.Parse(words[1]));
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message);
+}
+
+listOne.Sort();
+listTwo.Sort();
+
+var distances = new List<int>();
+for (int i = 0; i < lineCount; i++) 
+{
+  Console.WriteLine(listOne[i] + " - " + listTwo[i]);
+  int distance = Math.Abs(listOne[i]-listTwo[i]);
+  Console.WriteLine("Distance: " + distance);
+  distances.Add(distance);
+}
+int totalDistance = distances.Aggregate(0, (acc, x) => acc + x);
+Console.WriteLine("Total distance: " + totalDistance);
---
+++ day-1-pt-2/Program.cs
@@ -0,0 +1,55 @@
+﻿var listOne = new List<int>();
+var listTwo = new List<int>();
+int lineCount = 0;
+
+try
+{
+    using StreamReader reader = new("input.txt");
+
+    string text = reader.ReadToEnd();
+
+    string[] lines = text.Split(
+      new string[] { Environment.NewLine },
+      StringSplitOptions.None
+    );
+    lineCount = lines.Count();
+
+    foreach (var line in lines)
+    {
+        string[] words = System.Text.RegularExpressions.Regex.Split( line, @"\s{2,}");
+        listOne.Add(Int32.Parse(words[0]));
+        listTwo.Add(Int32.Parse(words[1]));
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message);
+}
+
+var frequencyDict = listTwo.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+/*foreach (KeyValuePair<int, int> author in frequencyDict)
+{
+    Console.WriteLine("Key: {0}, Value: {1}",author.Key, author.Value);
+}*/
+
+var similarityScores = new List<int>();
+foreach (var number in listOne)
+{
+  int frequency;
+  try 
+  {
+    frequency = frequencyDict[number];
+  }
+  catch (Exception e)
+  {
+    frequency = 0;
+  }
+  //Console.WriteLine("Num: " + number + " - " + frequency);
+  int similarityScore = number * frequency;
+  //Console.WriteLine("Num: " + number + " - " + similarityScore);
+  similarityScores.Add(similarityScore);
+}
+
+int totalsimilarityScore = similarityScores.Aggregate(0, (acc, x) => acc + x);
+Console.WriteLine("Total Similarity Score: " + totalsimilarityScore);
